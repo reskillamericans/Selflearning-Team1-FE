5c576919a8dca670fd2a897c4b5d032d0fef692c
krodri99 <krodri99@gmail.com>
2021-07-20 17:12:43 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Registration from './registration';
+import registration from './registration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Registration />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
