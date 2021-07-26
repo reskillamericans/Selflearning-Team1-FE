5cf0c5ee8b4594b2cf6c0d8817b8e06fb49f0e48
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-07-26 07:39:26 +0100

    bug fixes and routers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react"
-import MentorProfile from "./component/MentorProfile/MentorProfile"
+import React from "react";
+import MentorProfile from "./component/MentorProfile/MentorProfile";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <MentorProfile />
+      <Router>
+        <Switch>
+          <Route exact path="/profile" component={MentorProfile} />
+        </Switch>
+      </Router>
     </div>
-  )
-
+  );
 }
 
-export default App
-
+export default App;
